1e98fd2318de65b6f2efd52d5fd28b89ae26c08d
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2021-11-02 15:20:31 +0900

    03_ second version
    03 color Array Second version

--- 03_color_Array/index.js
+++ 03_color_Array/index.js
@@ -1,19 +1,10 @@
-const js = document.querySelector("span");
-const perspective = document.querySelector("#perspective");
-const blur = document.querySelector("#blur");
-const color = document.querySelector("#color");
-const imgContainer = document.querySelector(".city__container");
-const cityImg = document.querySelector("#city");
+const style = document.documentElement.style;
+const controller = document.querySelector(".controller__wrapper");
 
-perspective.addEventListener("input", (e) => {
-  cityImg.style.transform = `perspective(100px) translateZ(-${e.target.value}px)`;
-});
-
-blur.addEventListener("input", (e) => {
-  cityImg.style.filter = `blur(${e.target.value}px)`;
-});
-
-color.addEventListener("input", (e) => {
-  js.style.color = e.target.value;
-  imgContainer.style.boxShadow = `0px 0px 10px 20px ${e.target.value}`;
+controller.addEventListener("input", (e) => {
+  e.stopPropagation();
+  style.setProperty(
+    `--${e.target.dataset.property}`,
+    e.target.value + `${isNaN(e.target.value) ? "" : "px"}`
+  );
 });

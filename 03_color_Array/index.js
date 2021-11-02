0cf0df01a0d9a5f6e8eeb0c73128573e32fdb5ab
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2021-11-02 15:57:22 +0900

    03_second version
    03_second version

--- 03_color_Array/index.js
+++ 03_color_Array/index.js
@@ -2,7 +2,6 @@ const style = document.documentElement.style;
 const controller = document.querySelector(".controller__wrapper");
 
 controller.addEventListener("input", (e) => {
-  e.stopPropagation();
   style.setProperty(
     `--${e.target.dataset.property}`,
     e.target.value + `${isNaN(e.target.value) ? "" : "px"}`

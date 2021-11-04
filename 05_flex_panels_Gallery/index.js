70cf85b63dee82cd189bcf9ca6a0df7ed7d8a1f4
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2021-11-04 16:21:53 +0900

    05_ 2nd version
    error fixing + flex animation

--- 05_flex_panels_Gallery/index.js
+++ 05_flex_panels_Gallery/index.js
@@ -1,9 +1,7 @@
 const OPEN = "open";
 const panelContainer = document.querySelector(".gallery__wrapper");
-const panelList = document.querySelectorAll(".panel");
+const panel = document.querySelector(".panel");
 
 panelContainer.addEventListener("click", (e) => {
-  const targerEle = e.target;
-  if (targerEle.nodeName === "P") return;
-  targerEle.classList.toggle(OPEN);
+  e.target.closest(".panel").classList.toggle(OPEN);
 });

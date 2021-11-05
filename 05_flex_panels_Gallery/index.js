da05802182b0c5f9254593cd679626594367d4cc
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2021-11-05 17:21:31 +0900

    05_2nd version
    delete unused line

--- 05_flex_panels_Gallery/index.js
+++ 05_flex_panels_Gallery/index.js
@@ -1,6 +1,5 @@
 const OPEN = "open";
 const panelContainer = document.querySelector(".gallery__wrapper");
-const panel = document.querySelector(".panel");
 
 panelContainer.addEventListener("click", (e) => {
   e.target.closest(".panel").classList.toggle(OPEN);
